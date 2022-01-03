e461d150c51b51c7592a07967de68a0f4e98b2c2
ZZHZZHZZH <zhangzheheng12345@163.com>
2022-01-03 14:42:19 +0800

    Update README.md and add report

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Lightest, the name of the project, means the project provides a lightest C++ uni
 ## Usage 
 You only need to add lightest.h to your project, and include it in test files. 
  
-* Use macro DEFTEST(name) to define a test named 'name', and use macro RUNTEST(name) to start the test named 'name'. 
+* Use macro DEFTEST(name) to define a test named 'name', and use macro RUNTEST(name) to start the test named 'name'. Use macro REPORTTEST() to output a test list. 
 * Use macro MSG(str), WARN(str), ERR(str) and FAIL(str) to output test information. ERR(str) and FAIL(str) will set the test failed. 
-* Use macro REQUIRE(condition) to check a condition. If the condition is true, the macro will send a Msg, or if will send a Fail.
+* Use macro REQUIRE(condition) to check a condition. If the condition is failed, the macro will send a Fail, or it will do nothing. 
+* Use macro CHECK(condition) to check a condition. If the condition is true, the macro will send a Msg, or if will send a Fail. 
 * You must add a semicolon after a lightest macro. 
 * You should define a test before start the test. 
--- lightest.h
+++ lightest.h
@@ -5,38 +5,53 @@
 
 #define DEFTEST(name) void (*name)(Testing&&) = [] (Testing&& testing) 
 #define RUNTEST(name) (*name)(Testing( __FILE__, #name ))
+#define REPORTTEST() Testing.Report()
 
 class Testing {
     public:
         Testing(const char* file,const char* name) {
             std::cout << " [Begin] " << name << " ==========" << std::endl;
-            this->name = name, this->file = file;
-            this->failureCount = 0, this->failed = false;
+            test.name = name, test.file = file;
+            test.failureCount = 0, test.failed = false;
         }
         void Msg(int line,const char* str) {
-            std::cout << " [Message] " << file << ":" << line << ": " << str << std::endl;
+            std::cout << " [Message] " << test.file << " : " << line << " :  " << str << std::endl;
         }
         void Warn(int line,const char* str) {
-            std::cout << " [Warn] " << file << ":" << line << ": " << str << std::endl;
+            std::cout << " [Warn] " << test.file << " : " << line << " :  " << str << std::endl;
         }
         void Err(int line,const char* str) {
-            std::cout << " [Error] " << file << ":" << line << ": " << str << std::endl;
-            this->failed = true, this->failureCount++;
+            std::cout << " [Error] " << test.file << " : " << line << ":  " << str << std::endl;
+            test.failed = true, test.failureCount++;
         }
         void Fail(int line,const char* str) {
-            std::cout << " [Fail] " << file << ":" << line << ": " << str << std::endl;
-            this->failed = true, this->failureCount++;
+            std::cout << " [Fail] " << test.file << " : " << line << " :  " << str << std::endl;
+            test.failed = true, test.failureCount++;
         }
         ~Testing() {
             std::cout << " [End] " << this->name;
             if(this->failed) std::cout << " { " << this->failureCount << " Failure } ";
             std::cout << " ==========" <<std::endl;
+            test.push_back(test);
+        }
+        static void Report() {
+            std::cout << "[Report] ==========" << std::endl;
+            for(Test item : tests) {
+                std::cout << "| " << item.file << " : " << item.name << " :  " <<
+                          << item.failureCount << " failure " << std::endl;
+            }
+            std::cout << "[Report] ==========" << std::endl;
         }
     private:
-        const char* file;
-        const char* name;
-        unsigned int failureCount;
-        bool failed;
+        class Test {
+            public:
+                const char* file;
+                const char* name;
+                unsigned int failureCount;
+                bool failed;
+        };
+        Test test;
+        static std::vector<Test> tests;
 };
 
 #define MSG(str) testing.Msg(__LINE__,(str))
